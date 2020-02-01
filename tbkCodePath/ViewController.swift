5e4d25c692a8b61e99f86575a428fe3b74c5d204
tbkreations <trae0714@gmail.com>
2020-01-31 19:04:53 -0500

    Initial Commit of Project Files

--- tbkCodePath/ViewController.swift
+++ tbkCodePath/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var homeBtn: UIButton!
+    @IBOutlet weak var westBtn: UIButton!
+    @IBOutlet weak var eastBtn: UIButton!
+    
+   
+    @IBAction func tapHome(_ sender: Any) {
+      textLabel.text = "This is Hip Hop"
+      textLabel.textColor = UIColor.black
+      homeBtn.backgroundColor = UIColor.black
+      westBtn.backgroundColor = UIColor.black
+      eastBtn.backgroundColor = UIColor.black
+      imageView.image = UIImage(named: "vinyl")
+      self.view.backgroundColor = UIColor.white
+    }
+    @IBAction func tapWestCoast(_ sender: Any) {
+        textLabel.text = "Hello from K. Dot"
+        textLabel.textColor = UIColor.lightGray
+        homeBtn.backgroundColor = UIColor.black
+        westBtn.backgroundColor = UIColor.black
+        eastBtn.backgroundColor = UIColor.black
+        imageView.image = UIImage(named: "kendrick")
+        self.view.backgroundColor = UIColor.darkGray
+    }
+    @IBAction func tapEastCoast(_ sender: Any) {
+        textLabel.text = "Hello from J. Cole"
+        textLabel.textColor = UIColor.orange
+        homeBtn.backgroundColor = UIColor.black
+        westBtn.backgroundColor = UIColor.black
+        eastBtn.backgroundColor = UIColor.black
+        imageView.image = UIImage(named: "cole")
+        self.view.backgroundColor = UIColor.red
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
